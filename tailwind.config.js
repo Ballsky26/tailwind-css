3fca89bdab5ac1a2342c138e5231915795228794
Ballsky22 <ballskyteam22@gmail.com>
2023-02-22 23:29:10 +0700

    Menambahkan Functions & Directives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        13: "3.25rem",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      colors: {
+        ballsky: "#bada55",
+        kopi: "#c0ffee",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        goyang: "goyang 1s ease-in-out infinite",
+      },
+      keyframes: {
+        goyang: {
+          "0%, 100%": {
+            transform: "rotate (-3deg)",
+          },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
